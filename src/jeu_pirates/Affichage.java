bf17a3151122af6758af3aab4c63869403537593
BASTIDE Guillaume <guillaume.bastide@univ-tlse3.fr>
2024-03-12 18:13:01 +0100

    Affichage

--- src/jeu_pirates/Affichage.java
+++ src/jeu_pirates/Affichage.java
@@ -1,5 +1,17 @@
 package jeu_pirates;
 
 public class Affichage {
-
+    public static void afficherPlateau(Plateau plateau) {
+        int[][] cases = plateau.getCases();
+        int nbLignes = plateau.getNbLignes();
+        int nbColonnes = plateau.getNbColonnes();
+        
+        for (int ligne = 0; ligne < nbLignes; ligne++) {
+            for (int colonne = 0; colonne < nbColonnes; colonne++) {
+                System.out.print(cases[ligne][colonne] + "\t");
+            }
+            System.out.println();
+        }
+    }
 }
+
