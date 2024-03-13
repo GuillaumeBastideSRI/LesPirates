b48f3900e4c6033aa73ee162a8b401166f8c108b
GuillaumeBastideSRI <guillaume.bastide@univ-tlse3.fr>
2024-03-13 08:30:05 +0100

    Actualisation du Main et du Plateau

--- src/jeu_pirates/Main.java
+++ src/jeu_pirates/Main.java
@@ -1,14 +1,19 @@
 package jeu_pirates;
 
 public class Main {
-    public static void main(String[] args) {
-        Plateau plateau = new Plateau(6, 5);
-        De de = new De(6);
-        Pion pionRouge = new Pion("Rouge");
-        int deResultat = de.lancer();
-        System.out.println("Le résultat du dé est : " + deResultat);
-        pionRouge.avancer(deResultat);
-        System.out.println("Le pion rouge est maintenant à la case : " + pionRouge.getPosition());
-        plateau.afficherPlateauAvecPion(pionRouge);
-    }
+	public static void main(String[] args) {
+		Plateau plateau = new Plateau(6, 5);
+		De de = new De(6);
+		Pion pionRouge = new Pion("Rouge");
+		int deResultatRouge = de.lancer();
+		System.out.println("Le résultat du dé du joueur rouge est : " + deResultatRouge);
+		pionRouge.avancer(deResultatRouge);
+		System.out.println("Le pion rouge est maintenant à la case : " + pionRouge.getPosition());
+		Pion pionBleu = new Pion("Bleu");
+		int deResultatBleu = de.lancer();
+		System.out.println("Le résultat du dé du joueur bleu est : " + deResultatBleu);
+		pionBleu.avancer(deResultatBleu);
+		System.out.println("Le pion bleu est maintenant à la case : " + pionBleu.getPosition());
+		plateau.afficherPlateauAvecPion(pionRouge);
+	}
 }
