070ea8d08c30c41698f81f00aece9425525caca5
GuillaumeBastideSRI <guillaume.bastide@univ-tlse3.fr>
2024-03-13 09:19:07 +0100

    Position Kraken + Actualisation Main

--- src/jeu_pirates/Kraken.java
+++ src/jeu_pirates/Kraken.java
@@ -1,5 +1,16 @@
 package jeu_pirates;
 
+import java.util.Random;
+
 public class Kraken {
-	
+    private int nbCases;
+
+    public Kraken(int nbCases) {
+        this.nbCases = nbCases;
+    }
+
+    public int PositionKrakenAleatoire() {
+        Random rand = new Random();
+		return rand.nextInt(nbCases) + 1;
+    }
 }
