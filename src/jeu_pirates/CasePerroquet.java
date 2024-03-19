f7ad52fe16baf62d5980f4352759062b9de1c251
GuillaumeBastideSRI <guillaume.bastide@univ-tlse3.fr>
2024-03-19 11:54:01 +0100

    Perroquet + Kraken

--- src/jeu_pirates/CasePerroquet.java
+++ src/jeu_pirates/CasePerroquet.java
@@ -1,22 +1,37 @@
 package jeu_pirates;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Random;
 
 public class CasePerroquet {
     private int nbCases;
     private int positionPerroquet;
+    private List<Pion> joueursInvincibles;
 
     public CasePerroquet(int nbCases) {
         this.nbCases = nbCases;
-        this.positionPerroquet = PositionPerroquetAleatoire();
+        this.positionPerroquet = positionPerroquetAleatoire();
+        this.joueursInvincibles = new ArrayList<>();
     }
 
-    private int PositionPerroquetAleatoire() {
-        Random rand = new Random();
-        return rand.nextInt(nbCases) + 1;
+    private int positionPerroquetAleatoire() {
+        return new Random().nextInt(nbCases) + 1;
     }
 
     public int getPositionPerroquet() {
         return positionPerroquet;
+    }
+
+    public void rendreInvincible(Pion joueur) {
+        joueursInvincibles.add(joueur);
+    }
+
+    public boolean estInvincible(Pion joueur) {
+        return joueursInvincibles.contains(joueur);
+    }
+
+    public void retirerInvincibilite(Pion joueur) {
+        joueursInvincibles.remove(joueur);
     }
 }
