035f63cf307875158bcb9c0d77063b1028716ec6
GuillaumeBastideSRI <156887360+GuillaumeBastideSRI@users.noreply.github.com>
2024-04-01 19:23:02 +0200

    Update CasesSpeciales.java

--- src/jeu_pirates/CasesSpeciales.java
+++ src/jeu_pirates/CasesSpeciales.java
@@ -1,17 +1,17 @@
 package jeu_pirates;
 
-public interface CasesSpeciales {
-	int getPositionKraken();
-
-	int getPositionPerroquet();
-
-	int getPositionPotion();
-
-	void rendreInvincible(Pion joueur);
-
-	boolean estInvincible(Pion joueur);
-
-	void retirerInvincibilite(Pion joueur);
-
-	void utiliser(Pion joueur);
+public interface CaseSpéciales {
+	public interface Kraken {
+	    int getPositionKraken();
+	}
+	public interface Perroquet {
+	    int getPositionPerroquet();
+	    int getPositionPotion();
+	    void rendreInvincible(Pion joueur);
+	    boolean estInvincible(Pion joueur);
+	    void retirerInvincibilite(Pion joueur);
+	}
+	public interface Potion {
+	    void utiliser(Pion joueur);
+	}
 }
