b6e2ea808c6664985aaaf8f49d0cd2ebdbefe5ea
GuillaumeBastideSRI <156887360+GuillaumeBastideSRI@users.noreply.github.com>
2024-04-01 19:21:18 +0200

    Update Case.java

--- src/jeu_pirates/Case.java
+++ src/jeu_pirates/Case.java
@@ -1,21 +1,9 @@
 package jeu_pirates;
 
-import java.util.Random;
-
-public abstract class Case {
-    protected int nbCases;
-    protected int position;
-
-    public Case(int nbCases) {
-        this.nbCases = nbCases;
-        this.position = genererPositionAleatoire();
-    }
-
-    protected int genererPositionAleatoire() {
-        return new Random().nextInt(nbCases) + 1;
-    }
-
-    public int getPosition() {
-        return position;
-    }
+public class Case {
+	private  int numero;
+	
+	public Case(int numero) {
+		this.numero = numero;
+	}
 }
