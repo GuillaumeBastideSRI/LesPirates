4d75cc1ae36eb340f96176aa83c85eec47be07a3
GuillaumeBastideSRI <156887360+GuillaumeBastideSRI@users.noreply.github.com>
2024-04-01 19:21:59 +0200

    Update CasePerroquet.java

--- src/jeu_pirates/CasePerroquet.java
+++ src/jeu_pirates/CasePerroquet.java
@@ -2,13 +2,35 @@ package jeu_pirates;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
-public class CasePerroquet extends Case {
+public class CasePerroquet {
+    private int nbCases;
+    private int positionPerroquet;
+    private int positionPotion;
     private List<Pion> joueursInvincibles;
 
     public CasePerroquet(int nbCases) {
-        super(nbCases);
+        this.nbCases = nbCases;
+        this.positionPerroquet = positionPerroquetAleatoire();
+        this.positionPotion = positionPotionAleatoire();
         this.joueursInvincibles = new ArrayList<>();
+    }
+
+    private int positionPerroquetAleatoire() {
+        return new Random().nextInt(nbCases) + 1;
+    }
+
+    private int positionPotionAleatoire() {
+        int positionPotion;
+        do {
+            positionPotion = new Random().nextInt(nbCases) + 1;
+        } while (positionPotion == positionPerroquet);
+        return positionPotion;
+    }
+
+    public int getPositionPerroquet() {
+        return positionPerroquet;
     }
 
     public void rendreInvincible(Pion joueur) {
@@ -23,4 +45,7 @@ public class CasePerroquet extends Case {
         joueursInvincibles.remove(joueur);
     }
     
+    public int getPositionPotion() {
+        return positionPotion;
+    }
 }
